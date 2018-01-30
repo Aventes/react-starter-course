b4b9a86a6fae174e65f03294718788f70e82ca6c
Maksym Yurin <maksym.yurin@akvelon.com>
2018-01-30 13:59:10 +0200

    ReactJs remove files from default catalog

--- src/components/welcome/Hello.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Hello = ({name}) => <p>Hello {name}</p>;
-
-export default Hello;
--- src/components/welcome/InputField.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const InputField = ({onChange, value}) =>
-    <input onChange={onChange} value={value}/>
-;
